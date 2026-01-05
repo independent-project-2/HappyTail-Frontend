46e7b25558269a8c8834f77baec89a860ac0d2a5
imalkadilakshan99 <imalkadilakshan99@gmail.com>
2026-01-05 17:51:44 +0530

    Start project

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import Loading from './pages/loading'
 import Navbar from './components/nav'
 import Chatbot from './pages/chatbot'
+import BrowsePets from './pages/BrowsePets'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <BrowsePets/>
      
     </>
   )
---
+++ src/pages/BrowsePets.tsx
@@ -0,0 +1,12 @@
+export default function BrowsePets() {
+    return(
+        <>
+        <div className="flex flex-raw">
+            <div className="">Imalka</div>
+            <div className="">Imalka</div>
+            <div className="">Imalka</div>
+            <div className="">Imalka</div>
+        </div>
+        </>
+    );
+}
