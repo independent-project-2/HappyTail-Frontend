abc42468d3424050b4615c06b3414368f36d85e4
AThusithaGayan <thusithag@aplicy.com>
2026-01-13 20:41:38 +0530

    Revert "Delete src/App.tsx"
    This reverts commit e8c1698c2f552238f29109505d148507da60a25c.

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Loading from './pages/loading';
+import Navbar from './components/nav';
+import Chatbot from './pages/chatbot';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        {/* Loading page */}
+        <Route path="/" element={<Loading />} />
+        
+        {/* Authentication Routes */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        
+        {/* Chatbot Route */}
+        <Route path="/ask-me" element={<Chatbot />} />
+        
+        {/* Redirect old routes to loading */}
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="/browse-pets" element={<Navigate to="/" replace />} />
+        <Route path="/blog" element={<Navigate to="/" replace />} />
+        <Route path="/about" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
