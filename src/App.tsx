73eb65fa98f90ed4d01dc0a3d82ce5cad6b7c4fa
Daniru Dayarathna <168005576+danirudayarathna@users.noreply.github.com>
2026-01-13 18:54:01 +0530

    fix : fix some err

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Loading from './pages/loading'
 import Navbar from './components/nav'
 import Chatbot from './pages/chatbot'
@@ -9,14 +9,16 @@ import Blog from './pages/blog'
 function App() {
 
   return (
-    <BrowserRouter>
+    
+      <>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Loading />} />
         <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
-    </BrowserRouter>
+      </>
+    
   )
 }
 
